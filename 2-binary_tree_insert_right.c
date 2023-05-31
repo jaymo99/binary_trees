a2461a088626cd26cbce1119ac2d54aa507bfc7c
James Kariuki <karisjaymo99@gmail.com>
2023-05-31 06:32:25 +0300

    check if new_node is NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,14 +11,12 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
-	if (parent == NULL)
+	if (parent == NULL || new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	new_node = binary_tree_node(parent, value);
 
 	new_node->right = parent->right;
 	if (parent->right)
